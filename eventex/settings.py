863a69b32833aadb50a73ac25525f211df198b80
Thiago Assis <thiago.medassis@gmail.com>
2022-06-14 18:11:19 -0300

    Admin magic

--- eventex/settings.py
+++ eventex/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'test_without_migrations',
     'django_extensions',
     'eventex.core',
-    'eventex.subscriptions',
+    'eventex.subscriptions.apps.SubscriptionsConfig',
 ]
 
 MIDDLEWARE = [
