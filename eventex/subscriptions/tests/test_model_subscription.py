9ccf7b0046907633215cd18dca3aca5512ec831f
Thiago Assis <thiago.medassis@gmail.com>
2022-06-14 17:44:11 -0300

    Persiste a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from django.test import TestCase
+
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Thiago Assis',
+            cpf='12345678901',
+            email='thiago.medassis@gmail.com',
+            phone='21-996643040'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        self.assertIsInstance(self.obj.created_at, datetime)
